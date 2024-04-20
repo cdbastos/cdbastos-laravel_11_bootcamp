2d77af0d1c806ab6f64c30ea432613223f25f515
Cristian Bastos <cristianbastos14@gmail.com>
2024-04-20 17:48:25 -0500

    Update User seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,8 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+            'email_verified_at' => now(),
         ]);
     }
 }
